97c835b8e437fefd10406198668f40ba70e2cbda
Eric Dobbertin <eric@dairystatedesigns.com>
2017-11-12 15:15:30 -0600

    Publish 0.1.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "meteortesting:browser-tests",
   summary: "A helper package for Meteor test driver packages. Runs client tests in a headless browser.",
   git: "https://github.com/meteortesting/meteor-browser-tests.git",
-  version: '0.1.1',
+  version: '0.1.2',
   testOnly: true,
 });
 
