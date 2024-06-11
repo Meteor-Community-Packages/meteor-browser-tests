39f4173030648461a42749308db82b42d60e58f2
jankapunkt <jkuester@uni-bremen.de>
2024-06-11 10:48:19 +0200

    release 1.6.0-rc300.0

--- package.js
+++ package.js
@@ -3,12 +3,13 @@ Package.describe({
   summary:
     'A helper package for Meteor test driver packages. Runs client tests in a headless browser.',
   git: 'https://github.com/Meteor-Community-Packages/meteor-browser-tests.git',
-  version: '1.6.0-beta300.0',
+  version: '1.6.0-rc300.0',
   testOnly: true,
 });
 
 Package.onUse((api) => {
-  api.use('ecmascript@0.16.8-alpha300.11');
+  api.versionsFrom(['2.8.0', '3.0-rc.2'])
+  api.use('ecmascript');
 
   api.mainModule('server.js', 'server');
   api.addAssets(
